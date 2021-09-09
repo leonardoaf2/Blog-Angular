851b857fa6470cc23e9418388f5f89b3eb17da23
leonardoaf2 <leeo.affonso@hotmail.com>
2021-09-08 21:39:49 -0300

    Implementação do post e get de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 import { UsuarioLogin } from '../model/UsuarioLogin';
 import { Observable } from 'rxjs';
 import { Usuario } from '../model/Usuario';
@@ -13,6 +13,17 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token),
+  };
+
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+  }
+
+
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
     return this.http.post<UsuarioLogin>('https://blogleobe.herokuapp.com/usuarios/logar' ,
     usuarioLogin)
@@ -20,6 +31,10 @@ export class AuthService {
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('https://blogleobe.herokuapp.com/usuarios/cadastrar' ,
     usuario)
+  }
+
+  getByIdUser(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`https://blogleobe.herokuapp.com/usuarios/${id}`, this.token)
   }
 
   logado(){
